4462c20632dc6aff7f9d81e039c09b958e3b0297
ovimatei <ovidiumatei90@gmail.com>
2020-08-30 13:04:58 +0300

    Added image, productimage model

--- product/admin.py
+++ product/admin.py
@@ -4,3 +4,5 @@ from django.contrib import admin
 from product import models
 
 admin.site.register(models.Product)
+admin.site.register(models.Image)
+admin.site.register(models.ProductImage)
--- product/models.py
+++ product/models.py
@@ -11,3 +11,18 @@ class Product(models.Model):
     picture = models.CharField(
         default='https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg',
         max_length=255)
+
+
+class Image(models.Model):
+    url = models.CharField(
+        default='https://thumbs.dreamstime.com/b/no-thumbnail-image-placeholder-forums-blogs-websites-148010362.jpg',
+        max_length=255)
+    upload_to = models.ImageField()
+
+
+class ProductImage(models.Model):
+    all_product_names = [(product.id, product.name) for product in Product.objects.all()]
+    selected_product = models.IntegerField(choices=all_product_names, max_length=255, null=True, blank=True)
+    all_product_images = [(image.id, image.url) for image in Image.objects.all()]
+    selected_image = models.IntegerField(choices=all_product_images, max_length=255, null=True,
+                                         blank=True)
