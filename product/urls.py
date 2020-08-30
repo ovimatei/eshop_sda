553707fd3df788747653a83d71f31e6b4a7a02ff
ovimatei <ovidiumatei90@gmail.com>
2020-08-30 12:20:47 +0300

    Added product detail page

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from product.views import list_products, home_page, about
+from product.views import list_products, home_page, about, product_details
 
 urlpatterns =[
     path('products/', list_products, name="list_products"),
     path('', home_page, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('products/<id_>/', product_details, name='product_details'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from django.urls import resolve
+
+from product.models import Product
+
 
 def list_products(request):
-    return render(request, 'products.html')
+    current_url = resolve(request.path_info).url_name
+    all_products = Product.objects.all()
+    context = {
+        "products": all_products,
+        "current_url": current_url,
+    }
+    return render(request, 'products.html', context)
 
 
 def home_page(request):
@@ -15,5 +24,21 @@ def home_page(request):
 
     return render(request, 'home.html', context)
 
+
 def about(request):
     return render(request, 'about.html')
+
+
+def product_details(request, id_):
+    context = {}
+
+    products = Product.objects.filter(id=id_)
+    if len(products) > 0:
+        context['name'] = products[0].name
+        context['description'] = products[0].description
+        context['price'] = products[0].price
+        context['picture'] = products[0].picture
+    else:
+        return HttpResponse('Product not found')
+
+    return render(request, 'product_details.html', context)
